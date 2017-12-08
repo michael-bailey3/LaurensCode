8d3e5f76d5b8149a582a0c92de7caa69fc0f2fdf
Kris Davison <kris.davison@digital.hmrc.gov.uk>
2017-12-08 10:59:45 +0000

    Initial

---
+++ src/main/java/some/code/practice/Lesson1.java
@@ -0,0 +1,16 @@
+package some.code.practice;
+
+public class Lesson1 {
+
+    /**
+     * Method to check for eye colour and hair colour
+     * @param hairColour
+     * @param eyeColour
+     * @return true if hairColour = "Blonde" and eyeColour = "Blue"
+     */
+    public boolean hasBlondeHairAndBlueEyes(String hairColour, String eyeColour){
+        //
+        return false;
+    }
+
+}
---
+++ src/test/java/some/code/practice/TestLesson1.java
@@ -0,0 +1,42 @@
+package some.code.practice;
+
+import org.junit.Test;
+
+public class TestLesson1 {
+
+    private Lesson1 lesson1 = new Lesson1();
+
+    @Test
+    public void testBlondeHairAndBlueEyes_withCorrectInput() {
+
+    }
+
+    @Test
+    public void testBlondeHairAndBlueEyes_withWrongHairColour() {
+
+    }
+
+
+    @Test
+    public void testBlondeHairAndBlueEyes_withWrongEyeColour() {
+
+    }
+
+
+    @Test
+    public void testBlondeHairAndBlueEyes_withBothWrongInputs() {
+
+    }
+
+
+    @Test
+    public void testBlondeHairAndBlueEyes_withEmptyInputs() {
+
+    }
+
+    @Test
+    public void testBlondeHairAndBlueEyes_withNullInputs() {
+
+    }
+
+}
